052bcd75b858d4f10c35222a08a93acf54d2a84b
Pedro Flores <pedro.luquin7468@alumnos.udg.mx>
2020-09-29 12:51:51 -0500

    Captura nombre e imprime saludo

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(){
+    char nombre[100];
 
     printf("Hola mundo\n");
+    printf("Nombre: ");
+
+    fgets(nombre, sizeof(nombre), stdin);
+    printf("Hola %s", nombre);
 
     return 0;
 }
