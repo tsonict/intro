d359e313fe59735470547cd2bb6e8ac0f2f34665
Pedro Flores <pedro.luquin7468@alumnos.udg.mx>
2020-09-29 13:39:42 -0500

    Implementamos las funciones suma y resta

--- funciones.h
+++ funciones.h
@@ -1,14 +1,15 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-void suma(int a, int b){
-
+int suma(int a, int b){
+    return a+b;
 }
 
 
-void resta(int a, int b){
+int resta(int a, int b){
 
-    
+    return a-b;
+
 }
 
 #endif
